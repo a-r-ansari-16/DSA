09ab17aa799954e476ef8a7ef020d61576aa8a6a
a-r-ansari-16 <amanriyazansari0516@gmail.com>
2026-08-06 07:36:35 +0000

    Power function with time complexity O(n) and space complexity O(n)

---
+++ Recursion/Power_Function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int pow(int a, int b){
+    if(b==0) return 1;
+    else return a*pow(a,b-1);
+}
+int main(){
+    int a;
+    cout<<"Enter base: ";
+    cin>>a;
+    int b;
+    cout<<"Enter exponent: ";
+    cin>>b;
+    if(a==0 && b==0) cout<<"Not defined";
+    else cout<<"The value of "<<a<<" raised to the power "<<b<<" is "<<pow(a,b)<<endl;
+}
