23b17323e444250e1f5b5668989879e894519648
a-r-ansari-16 <amanriyazansari0516@gmail.com>
2026-08-06 07:37:40 +0000

    Printing 1 to n using extra parameter

---
+++ Recursion/Print_1_To_N_Extra_Parameter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void print(int n,int a){
+    if(a>n) return;
+    cout<<a<<endl;
+    print(n,a+1);
+}
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    print(n,1);
+}
